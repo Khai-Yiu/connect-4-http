01d9dede73fb4f5bcf8b1e2cad6a5d232b04f6ad
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-07-16 15:11:39 +0800

    user-integration > signup > given a user already exists with a given email > forbids creation of another user with that email

--- src/user.spec.ts
+++ src/user.spec.ts
@@ -3,13 +3,17 @@ import app from '@/app';
 
 describe('user-integration', () => {
     describe('signup', () => {
+        const user1Details = {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'john.doe@gmail.com'
+        };
+
         describe('given the user does not exist', () => {
             it('creates a user', async () => {
-                const response = await request(app).post('/user/signup').send({
-                    firstName: 'John',
-                    lastName: 'Doe',
-                    email: 'john.doe@gmail.com'
-                });
+                const response = await request(app)
+                    .post('/user/signup')
+                    .send(user1Details);
                 expect(response.statusCode).toBe(201);
                 expect(response.body).toEqual(
                     expect.objectContaining({
@@ -19,6 +23,19 @@ describe('user-integration', () => {
                         uuid: expect.toBeUuid()
                     })
                 );
+                expect(response.headers['content-type']).toMatch(/json/);
+            });
+        });
+        describe('given a user already exists with a given email', () => {
+            it('forbids creation of another user with that email', async () => {
+                await request(app).post('/user/signup').send(user1Details);
+                const response = await request(app)
+                    .post('/user/signup')
+                    .send(user1Details);
+                expect(response.statusCode).toBe(403);
+                expect(response.body.errors).toEqual([
+                    'A user with the email already exists'
+                ]);
                 expect(response.headers['content-type']).toMatch(/json/);
             });
         });
