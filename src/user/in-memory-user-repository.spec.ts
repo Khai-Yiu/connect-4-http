0326a54d415f53ab782a6cda0a9e8afd8f6587eb
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-07-16 15:36:24 +0800

    RED: in-memory-user-repository › given an email address › returns a list of users associated with the email

--- src/user/in-memory-user-repository.spec.ts
+++ src/user/in-memory-user-repository.spec.ts
@@ -1,14 +1,22 @@
 import InMemoryUserRepositoryFactory from '@/user/in-memory-user-repository';
 
 describe('in-memory-user-repository', () => {
+    const user1Details = {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john.doe@gmail.com'
+    };
+    const user2Details = {
+        firstName: 'John',
+        lastName: 'Doe 2',
+        email: 'john.doe@gmail.com'
+    };
+
     describe('given the details for a user who does not exist', () => {
         it('creates the user', async () => {
             const inMemoryUserRepository = new InMemoryUserRepositoryFactory();
-            const createdUser = await inMemoryUserRepository.create({
-                firstName: 'John',
-                lastName: 'Doe',
-                email: 'john.doe@gmail.com'
-            });
+            const createdUser =
+                await inMemoryUserRepository.create(user1Details);
             expect(createdUser).toEqual(
                 expect.objectContaining({
                     firstName: 'John',
@@ -17,6 +25,23 @@ describe('in-memory-user-repository', () => {
                     uuid: expect.toBeUuid()
                 })
             );
+        });
+    });
+    describe('given an email address', () => {
+        it('returns a list of users associated with the email', async () => {
+            const inMemoryUserRepository = new InMemoryUserRepositoryFactory();
+            await inMemoryUserRepository.create(user1Details);
+            await inMemoryUserRepository.create(user2Details);
+            const users =
+                await inMemoryUserRepository.findByEmail('john.doe@gmail.com');
+            expect(users).toEqual([
+                {
+                    firstName: 'John',
+                    lastName: 'Doe',
+                    email: 'john.doe@gmail.com',
+                    uuid: expect.toBeUuid()
+                }
+            ]);
         });
     });
 });
