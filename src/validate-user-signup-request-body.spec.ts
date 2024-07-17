879dda222f1f73e75829cfdca7aca0d288ff2611
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-07-17 14:25:02 +0800

    RED: validate-user-signup-request-body › given a user signup request body missing a field › fails validation

--- src/validate-user-signup-request-body.spec.ts
+++ src/validate-user-signup-request-body.spec.ts
@@ -15,4 +15,25 @@ describe('validate-user-signup-request-body', () => {
             expect(validationResult).toEqual({ isValid: true });
         });
     });
+    describe('given a user signup request body missing a field', () => {
+        it('fails validation', () => {
+            const userSignupRequestBody = {
+                firstName: 'Dung',
+                lastName: 'Eater',
+                email: 'dung.eater@gmail.com'
+            };
+            const validationResult = validateUserSignupRequestBody(
+                userSignupRequestBody
+            );
+            expect(validationResult).toEqual({
+                isValid: false,
+                errors: [
+                    {
+                        message: '"password" is required',
+                        path: 'password'
+                    }
+                ]
+            });
+        });
+    });
 });
