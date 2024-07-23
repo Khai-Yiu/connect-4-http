132866e30e09919dc882cedf92eeeb37240861b1
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-07-23 15:51:27 +0800

    RED: user-integration › login › given a user already exists › and they provide the correct credentials › they are provided with a session token

--- src/user-integration.spec.ts
+++ src/user-integration.spec.ts
@@ -1,10 +1,17 @@
-import request from 'supertest';
-import app from '@/app';
+import request, { Response } from 'supertest';
+import appFactory from '@/app';
+import { generateKeyPair, jwtDecrypt } from 'jose';
+import { last, path, pipe, split } from 'ramda';
 
 describe('user-integration', () => {
     describe('signup', () => {
         describe('given the user does not exist', () => {
             it('creates a user', async () => {
+                const app = appFactory({
+                    routerParameters: {
+                        stage: 'test'
+                    }
+                });
                 const response = await request(app).post('/user/signup').send({
                     firstName: 'John',
                     lastName: 'Doe',
@@ -25,6 +32,11 @@ describe('user-integration', () => {
         });
         describe('given a user already exists with a given email', () => {
             it('forbids creation of another user with that email', async () => {
+                const app = appFactory({
+                    routerParameters: {
+                        stage: 'test'
+                    }
+                });
                 await request(app).post('/user/signup').send({
                     firstName: 'Kenny',
                     lastName: 'Pho',
@@ -46,6 +58,11 @@ describe('user-integration', () => {
         });
         describe('given invalid user details', () => {
             it('forbids creation of user', async () => {
+                const app = appFactory({
+                    routerParameters: {
+                        stage: 'test'
+                    }
+                });
                 const response = await request(app).post('/user/signup').send({
                     firstName: 'Dempsey',
                     email: 'dempsey.lamnington@gmail.com'
@@ -66,5 +83,69 @@ describe('user-integration', () => {
             });
         });
     });
-    describe('login', () => {});
+    describe('login', () => {
+        describe('given a user already exists', () => {
+            describe('and they provide the correct credentials', () => {
+                it('they are provided with a session token', async () => {
+                    jest.useFakeTimers({
+                        doNotFake: ['setImmediate']
+                    });
+                    const dateInMilliseconds = Date.now();
+                    jest.setSystemTime(dateInMilliseconds);
+                    const {
+                        publicKey: jwtPublicKey,
+                        privateKey: jwtPrivateKey
+                    } = await generateKeyPair('PS256');
+                    const app = appFactory({
+                        routerParameters: {
+                            stage: 'test',
+                            keySet: {
+                                jwtPublicKey
+                            }
+                        }
+                    });
+                    const userDetails = {
+                        firstName: 'Dung',
+                        lastName: 'Eater',
+                        email: 'dung.eater@gmail.com',
+                        password: 'IAmTheDungEater'
+                    };
+                    await request(app).post('/user/signup').send(userDetails);
+                    const userCredentials = {
+                        username: 'dung.eater@gmail.com',
+                        password: 'IAmDungEater'
+                    };
+                    const publicKey = process.env.JWT_PUBLIC_KEY;
+                    const loginResponse = await request(app)
+                        .post('/user/login')
+                        .send(userCredentials);
+                    const jwt = pipe<[Response], string, Array<string>, string>(
+                        path(['header', 'authorization']),
+                        split(' '),
+                        last
+                    )(loginResponse);
+                    const { payload, protectedHeader } = await jwtDecrypt(
+                        jwt,
+                        jwtPrivateKey
+                    );
+                    const durationOfADayInSeconds = 1 * 24 * 60 * 60;
+                    const dateInSeconds = Math.trunc(dateInMilliseconds / 1000);
+                    expect(protectedHeader).toEqual({
+                        alg: 'RSA-OAEP-256',
+                        typ: 'JWT',
+                        enc: 'A256GCM'
+                    });
+                    expect(payload).toEqual({
+                        iss: 'connect4-http-server',
+                        iat: dateInSeconds,
+                        exp: dateInSeconds + durationOfADayInSeconds,
+                        sub: 'dung.eater@gmail.com',
+                        nbf: dateInSeconds,
+                        username: 'dung.eater@gmail.com',
+                        roles: []
+                    });
+                });
+            });
+        });
+    });
 });
