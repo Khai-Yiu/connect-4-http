e239d68e4700cbad7937f2228896e0627c2dffc7
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-07-23 15:51:53 +0800

    GREEN: user-integration › login › given a user already exists › and they provide the correct credentials › they are provided with a session token

--- src/user/user-router.ts
+++ src/user/user-router.ts
@@ -1,6 +1,8 @@
-import express from 'express';
-import UserService from '@/user/user-service';
+import express, { RequestHandler } from 'express';
+import UserService, { UserServiceInterface } from '@/user/user-service';
 import validateUserSignupRequestBody from '@/validate-user-signup-request-body';
+import { JwtPublicKey, KeySet } from '@/global';
+import { EncryptJWT } from 'jose';
 
 type User = {
     firstName: string;
@@ -9,26 +11,59 @@ type User = {
     password: string;
 };
 
-const userRouterFactory = (userService: UserService) => {
-    const userRouter = express.Router();
-    userRouter.post('/signup', (req, res, next) => {
+const loginRequestHandlerFactory = (
+    userService: UserServiceInterface,
+    jwtPublicKey?: JwtPublicKey
+): RequestHandler => {
+    return async (req, res, next) => {
+        const timeIssued = Date.now();
+        const durationOfDaysInMilliseconds = 1 * 24 * 60 * 60 * 1000;
+        const username = req.body.username;
+        const jwt = await new EncryptJWT({
+            username,
+            roles: []
+        })
+            .setProtectedHeader({
+                alg: 'RSA-OAEP-256',
+                typ: 'JWT',
+                enc: 'A256GCM'
+            })
+            .setIssuer('connect4-http-server')
+            .setIssuedAt()
+            .setExpirationTime('1 day from now')
+            .setNotBefore('0 sec from now')
+            .setSubject(username)
+            .encrypt(jwtPublicKey);
+
+        res.appendHeader('authorization', jwt).send();
+        next();
+    };
+};
+
+const registerRequestHandlerFactory =
+    (userService: UserService): RequestHandler =>
+    (req, res, next) => {
         const { isValid, errors } = validateUserSignupRequestBody(req.body);
 
         if (!isValid) {
             res.status(403).send({ errors });
         }
 
-        next();
-    });
-
-    userRouter.post('/signup', (req, res, next) => {
         const { firstName, lastName, email, password } = req.body;
         userService
             .create({ firstName, lastName, email, password })
             .then((user: User) => res.status(201).send(user))
             .catch((error) => res.status(403).send({ errors: [error.message] }))
             .catch(next);
-    });
+    };
+
+const userRouterFactory = (userService: UserService, keySet: KeySet) => {
+    const userRouter = express.Router();
+    userRouter.post('/signup', registerRequestHandlerFactory(userService));
+    userRouter.post(
+        '/login',
+        loginRequestHandlerFactory(userService, keySet?.jwtPublicKey)
+    );
 
     return userRouter;
 };
