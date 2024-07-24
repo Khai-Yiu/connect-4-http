d2f282e34ac6bff35181e6ccca39b8e0b6a77e35
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-07-24 16:16:47 +0800

    RED: get-is-user-authorized › given a token › which cannot be decrypted using the private key › returns false

---
+++ src/get-is-user-authorized.spec.ts
@@ -0,0 +1,14 @@
+import { generateKeyPair } from 'jose';
+import { getIsUserAuthorized } from '@/get-is-user-authorized';
+
+describe('get-is-user-authorized', () => {
+    describe('given a token', () => {
+        describe('which cannot be decrypted using the private key', () => {
+            it('returns false', async () => {
+                const { privateKey } = await generateKeyPair('RS256');
+                const token = '121321kek32di323f3';
+                expect(getIsUserAuthorized(token, privateKey)).toBe(false);
+            });
+        });
+    });
+});
