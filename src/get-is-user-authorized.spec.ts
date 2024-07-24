0233c43660aa885d8241f1c69baac79d576ced9b
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-07-24 16:24:16 +0800

    GREEN: get-is-user-authorized › given a token › which cannot be decrypted using the private key › returns false

--- src/get-is-user-authorized.spec.ts
+++ src/get-is-user-authorized.spec.ts
@@ -1,5 +1,5 @@
 import { generateKeyPair } from 'jose';
-import { getIsUserAuthorized } from '@/get-is-user-authorized';
+import getIsUserAuthorized from '@/get-is-user-authorized';
 
 describe('get-is-user-authorized', () => {
     describe('given a token', () => {
@@ -7,7 +7,9 @@ describe('get-is-user-authorized', () => {
             it('returns false', async () => {
                 const { privateKey } = await generateKeyPair('RS256');
                 const token = '121321kek32di323f3';
-                expect(getIsUserAuthorized(token, privateKey)).toBe(false);
+                expect(await getIsUserAuthorized(token, privateKey)).toBe(
+                    false
+                );
             });
         });
     });
