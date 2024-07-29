e8d7fe87bb7e8e26205459b940584e98ae412dcd
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-07-29 16:11:46 +0800

    RED: invite-integration › given an inviter that is an existing user › and an invitee that is an existing user › when the inviter sends an invitation to the invitee › creates an invitation

---
+++ src/invite/invite-integration.spec.ts
@@ -0,0 +1,60 @@
+import appFactory from '@/app';
+import { generateKeyPair } from 'jose';
+import request from 'supertest';
+import { App } from 'supertest/types';
+
+describe('invite-integration', () => {
+    let app: App;
+
+    beforeEach(async () => {
+        app = appFactory({
+            routerParameters: {
+                stage: 'test',
+                keySet: await generateKeyPair('RS256')
+            }
+        });
+    });
+
+    describe('given an inviter that is an existing user', () => {
+        describe('and an invitee that is an existing user', () => {
+            describe('when the inviter sends an invitation to the invitee', () => {
+                it('creates an invitation', async () => {
+                    jest.useFakeTimers({ doNotFake: ['setImmediate'] });
+                    const currentTime = Date.now();
+                    jest.setSystemTime(currentTime);
+
+                    const inviterDetails = {
+                        firstName: 'Player',
+                        lastName: 'Two',
+                        email: 'player1@gmail.com',
+                        password: 'Hello123'
+                    };
+                    const inviteeDetails = {
+                        firstName: 'Player',
+                        lastName: 'Two',
+                        email: 'player2@gmail.com',
+                        password: 'Hello123'
+                    };
+                    const inviterCredentials = {
+                        email: 'player1@gmail.com',
+                        password: 'Hello123'
+                    };
+                    await request(app).post('/signup').send(inviterDetails);
+                    await request(app).post('/signup').send(inviteeDetails);
+                    await request(app).post('/login').send(inviterCredentials);
+                    const response = await request(app).post('/invite').send({
+                        invitee: 'player2@gmail.com'
+                    });
+                    const lengthOfDayInMilliseconds = 60 * 60 * 24 * 1000;
+                    expect(response.statusCode).toBe(201);
+                    expect(response.body.invite).toEqual({
+                        uuid: expect.toBeUuid(),
+                        inviter: 'player1@gmail.com',
+                        invitee: 'player2@gmail.com',
+                        exp: currentTime + lengthOfDayInMilliseconds
+                    });
+                });
+            });
+        });
+    });
+});
