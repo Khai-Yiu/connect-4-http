9d0e5e751cf8074bc47c5b57b6aa81c6cd2c4063
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-07-30 14:36:57 +0800

    RED: invite-integration › given the inviter is not logged in › when the inviter sends an invitation › returns http status code 401

--- src/invite/invite-integration.spec.ts
+++ src/invite/invite-integration.spec.ts
@@ -19,47 +19,70 @@ describe('invite-integration', () => {
         });
     });
 
+    describe('given the inviter is not logged in', () => {
+        describe('when the inviter sends an invitation', () => {
+            it('returns http status code 401', async () => {
+                const inviteDetails = {
+                    invitee: 'player1@gmail.com',
+                    inviter: 'player2@gmail.com'
+                };
+                const response = await request(app)
+                    .post('/invite')
+                    .send(inviteDetails);
+                expect(response.statusCode).toBe(401);
+                expect(response.body.errors).toEqual([
+                    'You must be logged in to send an invite.'
+                ]);
+            });
+        });
+    });
     describe('given an inviter that is an existing user', () => {
-        describe('and an invitee that is an existing user', () => {
-            describe('when the inviter sends an invitation to the invitee', () => {
-                it('creates an invitation', async () => {
-                    jest.useFakeTimers({ doNotFake: ['setImmediate'] });
-                    const currentTime = Date.now();
-                    jest.setSystemTime(currentTime);
+        describe('and the inviter is logged in', () => {
+            describe('and an invitee that is an existing user', () => {
+                describe('when the inviter sends an invitation to the invitee', () => {
+                    it('creates an invitation', async () => {
+                        jest.useFakeTimers({ doNotFake: ['setImmediate'] });
+                        const currentTime = Date.now();
+                        jest.setSystemTime(currentTime);
 
-                    const inviterDetails = {
-                        firstName: 'Player',
-                        lastName: 'Two',
-                        email: 'player1@gmail.com',
-                        password: 'Hello123'
-                    };
-                    const inviteeDetails = {
-                        firstName: 'Player',
-                        lastName: 'Two',
-                        email: 'player2@gmail.com',
-                        password: 'Hello123'
-                    };
-                    const inviterCredentials = {
-                        email: 'player1@gmail.com',
-                        password: 'Hello123'
-                    };
-                    await request(app).post('/signup').send(inviterDetails);
-                    await request(app).post('/signup').send(inviteeDetails);
-                    await request(app).post('/login').send(inviterCredentials);
-                    const response = await request(app).post('/invite').send({
-                        inviter: 'player1@gmail.com',
-                        invitee: 'player2@gmail.com'
+                        const inviterDetails = {
+                            firstName: 'Player',
+                            lastName: 'Two',
+                            email: 'player1@gmail.com',
+                            password: 'Hello123'
+                        };
+                        const inviteeDetails = {
+                            firstName: 'Player',
+                            lastName: 'Two',
+                            email: 'player2@gmail.com',
+                            password: 'Hello123'
+                        };
+                        const inviterCredentials = {
+                            email: 'player1@gmail.com',
+                            password: 'Hello123'
+                        };
+                        await request(app).post('/signup').send(inviterDetails);
+                        await request(app).post('/signup').send(inviteeDetails);
+                        await request(app)
+                            .post('/login')
+                            .send(inviterCredentials);
+                        const response = await request(app)
+                            .post('/invite')
+                            .send({
+                                inviter: 'player1@gmail.com',
+                                invitee: 'player2@gmail.com'
+                            });
+                        const lengthOfDayInMilliseconds = 60 * 60 * 24 * 1000;
+                        expect(response.statusCode).toBe(201);
+                        expect(response.body.invite).toEqual({
+                            uuid: expect.toBeUuid(),
+                            inviter: 'player1@gmail.com',
+                            invitee: 'player2@gmail.com',
+                            exp: currentTime + lengthOfDayInMilliseconds,
+                            status: 'PENDING'
+                        });
+                        jest.useRealTimers();
                     });
-                    const lengthOfDayInMilliseconds = 60 * 60 * 24 * 1000;
-                    expect(response.statusCode).toBe(201);
-                    expect(response.body.invite).toEqual({
-                        uuid: expect.toBeUuid(),
-                        inviter: 'player1@gmail.com',
-                        invitee: 'player2@gmail.com',
-                        exp: currentTime + lengthOfDayInMilliseconds,
-                        status: 'PENDING'
-                    });
-                    jest.useRealTimers();
                 });
             });
         });
