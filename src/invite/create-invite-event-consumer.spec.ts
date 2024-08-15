94532cc37a213b55210254781c75a480403001b1
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-08-15 16:40:05 +0800

    RED: create-invite-event-consumer > given an event subscription > and an invitee notification function > when an 'invite_created' event is received > the inviter notifies the invitee

---
+++ src/invite/create-invite-event-consumer.spec.ts
@@ -0,0 +1,41 @@
+import { InviteEvents, InviteStatus } from '@/invite/invite-service.d';
+import { Subject } from 'rxjs';
+import createInviteEventListener, {
+    InviteCreatedEvent
+} from '@/invite/create-invite-event-consumer';
+
+describe('create-invite-event-consumer', () => {
+    describe('given an event subscription', () => {
+        describe('and an invitee notification function', () => {
+            describe('and an invite is sent', () => {});
+            const subscriptionFn = new Subject<InviteCreatedEvent>();
+            const notificationFn = jest.fn();
+            createInviteEventListener(subscriptionFn, notificationFn);
+
+            describe('when an "invite_created" event is received', () => {
+                it('the inviter notifies the invitee', () => {
+                    subscriptionFn.next({
+                        type: InviteEvents.INVITATION_CREATED,
+                        payload: {
+                            inviter: 'inviter@gmail.com',
+                            invitee: 'some@gmail.com',
+                            exp: Date.now(),
+                            uuid: crypto.randomUUID(),
+                            status: InviteStatus.PENDING
+                        }
+                    });
+                    expect(notificationFn).toHaveBeenCalledWith({
+                        recipient: 'some@gmail.com',
+                        payload: {
+                            inviter: 'inviter@gmail.com',
+                            invitee: 'some@gmail.com',
+                            exp: expect.any(Number),
+                            uuid: expect.toBeUuid(),
+                            status: InviteStatus.PENDING
+                        }
+                    });
+                });
+            });
+        });
+    });
+});
