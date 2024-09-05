5c569c62513241f80cff40ddf0c919b18bb48790
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-09-05 14:51:47 +0800

    RED: in-memory-session-repository › given a session has been created › when provided with the session ID › returns the session

--- src/session/in-memory-session-repository.spec.ts
+++ src/session/in-memory-session-repository.spec.ts
@@ -9,15 +9,17 @@ describe('in-memory-session-repository', () => {
 
     describe('given details about a session', () => {
         it('creates the session', () => {
-            const sessionDetails = {
+            const sessionCreationDetails = {
                 inviterUuid: '010f712b-7b31-420b-bd40-f23584fac148',
                 inviteeUuid: 'd36e91c5-5c0d-481d-b929-5dab0267257a'
             };
 
-            const createdSession =
-                inMemorySessionRepository.create(sessionDetails);
+            const createdSession = inMemorySessionRepository.create(
+                sessionCreationDetails
+            );
             expect(createdSession).toEqual(
                 expect.objectContaining({
+                    uuid: expect.toBeUuid(),
                     inviter: expect.objectContaining({
                         uuid: '010f712b-7b31-420b-bd40-f23584fac148'
                     }),
@@ -26,6 +28,32 @@ describe('in-memory-session-repository', () => {
                     })
                 })
             );
+        });
+    });
+    describe('given a session has been created', () => {
+        describe('when provided with the session ID', () => {
+            it('returns the session', () => {
+                const sessionCreationDetails = {
+                    inviterUuid: '010f712b-7b31-420b-bd40-f23584fac148',
+                    inviteeUuid: 'd36e91c5-5c0d-481d-b929-5dab0267257a'
+                };
+                const { uuid } = inMemorySessionRepository.create(
+                    sessionCreationDetails
+                );
+                const retrievedSession =
+                    inMemorySessionRepository.getSession(uuid);
+                expect(retrievedSession).toEqual(
+                    expect.objectContaining({
+                        uuid: expect.toBeUuid(),
+                        inviter: expect.objectContaining({
+                            uuid: '010f712b-7b31-420b-bd40-f23584fac148'
+                        }),
+                        invitee: expect.objectContaining({
+                            uuid: 'd36e91c5-5c0d-481d-b929-5dab0267257a'
+                        })
+                    })
+                );
+            });
         });
     });
 });
