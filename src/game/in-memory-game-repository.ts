71c9481bc755c6f7ddb0d1ec51bd048eb8e7dd64
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-09-05 16:25:10 +0800

    GREEN: in-memory-game-repository > saving a game > when given a game to save > saves the game-

--- src/game/in-memory-game-repository.ts
+++ src/game/in-memory-game-repository.ts
@@ -1,1 +1,25 @@
-export default class InMemoryGameRepository implements GameRepository {}
+import { Uuid } from '@/global';
+import { GameDetails, PersistedGameDetails } from './game-service.d';
+
+export interface GameRepository {
+    saveGame: (gameDetails: GameDetails) => Promise<PersistedGameDetails>;
+}
+
+export default class InMemoryGameRepository implements GameRepository {
+    games: Map<Uuid, PersistedGameDetails>;
+
+    constructor() {
+        this.games = new Map();
+    }
+
+    async saveGame(gameDetails: GameDetails) {
+        const uuid = crypto.randomUUID();
+        const persistedGameDetails = {
+            uuid,
+            ...gameDetails
+        };
+        this.games.set(uuid, persistedGameDetails);
+
+        return persistedGameDetails;
+    }
+}
