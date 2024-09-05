35ee249144746db8ea4927d4498e766ad3551230
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-09-05 15:10:40 +0800

    RED: session-service › creating a session › given the identities of two players › creates a session

--- src/session/session-service.spec.ts
+++ src/session/session-service.spec.ts
@@ -19,16 +19,22 @@ describe('session-service', () => {
         });
     });
     describe('creating a session', () => {
-        describe('given the ID of two players', () => {
-            it.skip('creates a session', () => {
-                const sessionID = sessionService.createSession({
-                    inviterUuid: 'Bob',
-                    inviteeUuid: 'Alice'
+        describe('given the identities of two players', () => {
+            it('creates a session', () => {
+                const sessionDetails = sessionService.createSession({
+                    inviterUuid: 'ac698d60-5f6e-4d89-be27-f12c9b054d22',
+                    inviteeUuid: 'd5bcfb7a-b8a8-4274-afef-c5db4509813a'
                 });
-                expect(sessionService.getSession(sessionID)).toEqual(
+
+                expect(sessionDetails).toEqual(
                     expect.objectContaining({
-                        inviterUuid: expect.objectContaining({ uuid: 'Bob' }),
-                        inviteeUuid: expect.objectContaining({ uuid: 'Alice' })
+                        uuid: expect.toBeUuid(),
+                        inviterUuid: expect.objectContaining({
+                            uuid: 'ac698d60-5f6e-4d89-be27-f12c9b054d22'
+                        }),
+                        inviteeUuid: expect.objectContaining({
+                            uuid: 'd5bcfb7a-b8a8-4274-afef-c5db4509813a'
+                        })
                     })
                 );
             });
