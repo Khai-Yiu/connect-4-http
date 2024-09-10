bdbcd69efe5f9e5844725615913310ee55ba8d03
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-09-10 16:06:14 +0800

    RED: in-memory-session-repository › updating session details › given the uuid of an existing session › and the uuid of a game to set as the active game › sets the active game of the session

--- src/session/in-memory-session-repository.spec.ts
+++ src/session/in-memory-session-repository.spec.ts
@@ -75,6 +75,23 @@ describe('in-memory-session-repository', () => {
                     expect(sessionDetails.gameUuids).toEqual([gameUuid]);
                 });
             });
+            describe('and the uuid of a game to set as the active game', () => {
+                it('sets the active game of the session', async () => {
+                    const gameUuid = 'c944743d-3f06-4ee6-a697-669e3cb02655';
+                    const { uuid } = await inMemorySessionRepository.create({
+                        inviterUuid: '0dbb264d-3979-483a-a579-e849d79e9c85',
+                        inviteeUuid: '02ca8ef5-716d-4421-92e4-ff94682d0b33'
+                    });
+                    await inMemorySessionRepository.setActiveGame(
+                        uuid,
+                        gameUuid
+                    );
+                    const sessionDetails =
+                        await inMemorySessionRepository.getSession(uuid);
+
+                    expect(sessionDetails.activeGameUuid).toBe(gameUuid);
+                });
+            });
         });
     });
 });
